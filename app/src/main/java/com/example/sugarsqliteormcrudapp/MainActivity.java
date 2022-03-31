bb753b40eeff638edf7cfd4752bd72e75df41d30
Leo <Leo5801>
2022-03-31 15:43:42 +0300

    axaxaxa

--- app/src/main/java/com/example/sugarsqliteormcrudapp/MainActivity.java
+++ app/src/main/java/com/example/sugarsqliteormcrudapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.sugarsqliteormcrudapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.setting:
+                Toast.makeText(MainActivity.this, "Setting Menu", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.insert:
+                startActivity(new Intent(MainActivity.this, InsertActivity.class));
+                break;
+        }
+        // asdas
+
+        return super.onOptionsItemSelected(item);
     }
 }
